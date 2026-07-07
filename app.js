bb98aa18f864ff1d70de76f23bea102f102d6cfe
proGuilhermeCamargo <guilherme.camargo@pro.fecaf.com.br>
2026-07-07 19:28:59 -0300

    fix: criacao da logica inicial

--- app.js
+++ app.js
@@ -2,14 +2,11 @@
 
 
 function calcularTotal (itens) {
-    let total = 0;
+    let total = 0
 
-    for (let i = 0; i < itens.length; i++) {
+    for (let i = 0; i < itens.length; i++){
         total += itens[i].preco * 1.1
     }
 
-    // Aplica desconto de fidelidade
-    // Antes de retornar o valor final
-
-    return total;
+   return total
 }
