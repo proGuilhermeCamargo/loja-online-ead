38b1555a655c79c2d4490f5c1c94f2645b6fbd7c
proGuilhermeCamargo <guilherme.camargo@pro.fecaf.com.br>
2026-07-07 19:10:31 -0300

    fix: ajustes na logica de calculo

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ function calcularTotal (itens) {
     let total = 0;
 
     for (let i = 0; i < itens.length; i++) {
-        total += itens[i].preco
+        total += itens[i].preco * 1.1
     }
 
     return total;
