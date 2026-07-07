af2d35fa3266441f36d95261bc15f71612d46be5
proGuilhermeCamargo <guilherme.camargo@pro.fecaf.com.br>
2026-07-07 19:04:37 -0300

    calcula valor no appjs

--- app.js
+++ app.js
@@ -2,14 +2,11 @@
 
 
 function calcularTotal (itens) {
-    let total = 0 
+    let total = 0;
 
     for (let i = 0; i < itens.length; i++) {
         total += itens[i].preco
     }
 
-    // aplica desconto de fidelidade
-    // Isso e um teste
-
-    return total
+    return total;
 }
